155aca7ba70f7b5509a99a000b84e532e94bc4d3
hunghg255 <giahunghust@gmail.com>
2023-01-13 14:56:53 +0700

    update i18n

---
+++ src/i18n.ts
@@ -0,0 +1,20 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import Languagedetector from 'i18next-browser-languagedetector';
+import HttpApi from 'i18next-http-backend';
+
+i18n
+  .use(Languagedetector)
+  .use(HttpApi)
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    ns: ['common'],
+    supportedLngs: ['en', 'vi'],
+    fallbackNS: 'en',
+    interpolation: {
+      escapeValue: false, // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
+    },
+    backend: {
+      loadPath: 'locales/{{lng}}/{{ns}}.json',
+    },
+  });
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import 'antd/dist/reset.css';
 import ErrorBoundary from './components/ErrorBoundary';
+
+import './i18n';
+
+import 'antd/dist/reset.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <ErrorBoundary>
