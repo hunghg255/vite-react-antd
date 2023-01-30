411db6555752f73f15849f5629e41fff0b7d9555
hunghg255 <giahunghust@gmail.com>
2023-01-30 15:11:52 +0700

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -6,43 +6,48 @@ import * as path from 'path';
 import { antdDayjs } from 'antd-dayjs-vite-plugin';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    antdDayjs(),
-    EnvironmentPlugin('all'),
-    //  resolve({ "react-codemirror2": `
-    //       const UnControlled = {};
-    //       export {
-    //         UnControlled,
-    //       }`
-    //   }
-    checker({
-      typescript: true,
-    }),
-  ],
-  css: {
-    devSourcemap: true,
-  },
-  optimizeDeps: {
-    include: ['react'],
-  },
-  build: {
-    commonjsOptions: {
-      include: [/node_modules/],
+export default defineConfig(({ mode }) => {
+  const isDev = mode !== 'production';
+  const isAnalyze = mode === 'analyze';
+
+  return {
+    plugins: [
+      react(),
+      antdDayjs(),
+      EnvironmentPlugin('all'),
+      //  resolve({ "react-codemirror2": `
+      //       const UnControlled = {};
+      //       export {
+      //         UnControlled,
+      //       }`
+      //   }
+      checker({
+        typescript: true,
+      }),
+    ],
+    css: {
+      devSourcemap: isDev,
     },
-    // sourcemap: true, // check analyze
-  },
-  resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
-  },
-  esbuild: {
-    sourcemap: true,
-  },
-  server: {
-    port: 3000,
-  },
-  preview: {
-    port: 3000,
-  },
+    optimizeDeps: {
+      include: ['react'],
+    },
+    build: {
+      commonjsOptions: {
+        include: [/node_modules/],
+      },
+      sourcemap: isAnalyze,
+    },
+    resolve: {
+      alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+    },
+    esbuild: {
+      sourcemap: isDev,
+    },
+    server: {
+      port: 3000,
+    },
+    preview: {
+      port: 3000,
+    },
+  };
 });
