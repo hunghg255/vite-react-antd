6676acda8d9e30de460f75471a4ffcfa1d327ebc
hunghg255 <giahunghust@gmail.com>
2023-02-08 23:43:19 +0700

    update

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import ErrorBoundary from './components/ErrorBoundary';
 
-import './i18n';
-
 import 'antd/dist/reset.css';
+import './styles/globals.scss';
+
+import './i18n';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <ErrorBoundary>
