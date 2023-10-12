881c5243120ae9e7d3938c0f885335c06cded575
hunghg255 <giahunghust@gmail.com>
2023-10-12 11:29:19 +0700

    chore: update

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,10 +1,5 @@
-import React from 'react';
-
-import { Form, Input } from 'antd';
 import { Helmet } from 'react-helmet';
-import { Link } from 'react-router-dom';
 
-import ButtonPrimary from '@/components/UI/Button/ButtonPrimary';
 import Text from '@/components/UI/Text';
 // import { useAuth } from '@/store/auth/useAuth';
 
@@ -20,51 +15,10 @@ const SignIn = () => {
       <h1>{process.env.TITLE}</h1>
 
       <div className={styles.loginWrap}>
-        <img src='public/svg/logo.svg' alt='logo' />
+        <img src='/svg/logo.svg' alt='logo' />
         <Text type='heading3-bold' className='text-center mb-20' element='h1'>
-          Đăng nhập vào IC Publisher
+          Đăng nhập vào IC Publisher 11
         </Text>
-        <Form layout='vertical'>
-          <Form.Item
-            name='email'
-            label='Email'
-            rules={[
-              {
-                required: true,
-                message: 'Vui lòng nhập email',
-              },
-              {
-                type: 'email',
-                message: 'Email không hợp lệ',
-              },
-            ]}
-          >
-            <Input placeholder='Nhập email' />
-          </Form.Item>
-
-          <Form.Item
-            name='password'
-            label='Mật khẩu'
-            rules={[
-              {
-                required: true,
-                message: 'Vui lòng nhập mật khẩu',
-              },
-              {
-                min: 6,
-                message: 'Mật khẩu phải có ít nhất 6 ký tự',
-              },
-            ]}
-          >
-            <Input placeholder='Mật khẩu' />
-          </Form.Item>
-
-          <Text type='heading5-regular' color='primary-light' className='mb-20 text-right'>
-            <Link to='/forgot-password'>Quên mật khẩu?</Link>
-          </Text>
-
-          <ButtonPrimary htmlType='submit'>Đăng nhập</ButtonPrimary>
-        </Form>
       </div>
     </>
   );
