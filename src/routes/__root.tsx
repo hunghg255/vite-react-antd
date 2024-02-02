f07cba7c0670f23c8d57b299a909fc961ca444a4
hunghg255 <giahunghust@gmail.com>
2024-02-03 00:49:46 +0700

    chore: update

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,21 @@
+/* eslint-disable indent */
+import React from 'react';
+
 import { createRootRoute, Outlet, ScrollRestoration } from '@tanstack/react-router';
-import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 
 import AppLayout from '@/layout/AppLayout/AppLayout';
+
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === 'production'
+    ? () => <></> // Render nothing in production
+    : React.lazy(() =>
+        // Lazy load in development
+        import('@tanstack/router-devtools').then((res) => ({
+          default: res.TanStackRouterDevtools,
+          // For Embedded Mode
+          // default: res.TanStackRouterDevtoolsPanel
+        })),
+      );
 
 export const Route = createRootRoute({
   component: () => (
