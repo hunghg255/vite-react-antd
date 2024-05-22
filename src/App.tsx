0620de5202cc3906b1c973bd12acacc8ecd72577
hunghg255 <giahunghust@gmail.com>
2024-05-22 12:36:50 +0700

    chore: update

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import { RouterProvider } from 'react-router-dom';
+import { Toaster } from 'sonner';
 
 import { router } from './routes/routes';
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <RouterProvider router={router} />
+      <Toaster position='top-center' />
+    </>
+  );
 }
 
 export default App;
--- src/components/UI/Text/index.tsx
+++ src/components/UI/Text/index.tsx
@@ -1,72 +1,33 @@
-/* eslint-disable @typescript-eslint/ban-types */
 import React from 'react';
 
 import classNames from 'classnames';
 
+import { TColor } from '@/styles/color/color.variables';
+
 interface TextProps {
   children: React.ReactNode;
   type?:
-    | 'heading1-medium'
-    | 'heading2-medium'
+    | 'heading1-extra-bold'
+    | 'heading2-bold'
     | 'heading3-bold'
-    | 'heading3-medium'
-    | 'heading3-regular'
-    | 'heading4-medium'
-    | 'heading5-bold'
+    | 'heading3-semi-bold'
+    | 'heading4-semi-bold'
     | 'heading5-medium'
-    | 'heading5-regular'
-    | 'body-bold'
-    | 'body-medium'
-    | 'body-regular'
-    | 'body-underline'
-    | 'caption-regular'
-    | (string & {});
-  color?:
-    | 'cwhite'
-    | 'cblack'
-    | 'primary-lighter'
-    | 'primary-light'
-    | 'primary-main'
-    | 'primary-dark'
-    | 'primary-darker'
-    | 'secondary-lightest'
-    | 'secondary-lighter'
-    | 'secondary-light'
-    | 'secondary-main'
-    | 'secondary-dark'
-    | 'neutral-white'
-    | 'neutral-gray-2'
-    | 'neutral-gray-3'
-    | 'neutral-gray-4'
-    | 'neutral-gray-5'
-    | 'neutral-gray-6'
-    | 'neutral-gray-7'
-    | 'neutral-gray-8'
-    | 'neutral-gray-9'
-    | 'neutral-gray-10'
-    | 'success-background'
-    | 'success-hover'
-    | 'success-main'
-    | 'success-active'
-    | 'warning-background'
-    | 'warning-hover'
-    | 'warning-main'
-    | 'warning-active'
-    | 'error-background'
-    | 'error-hover'
-    | 'error-main'
-    | 'error-active'
-    | 'link-1'
-    | 'link-2'
-    | 'link-3'
-    | 'link-4'
-    | 'link-5'
-    | (string & {});
+    | 'heading5-bold'
+    | 'heading6-bold'
+    | 'title1-semi-bold'
+    | 'title2-semi-bold'
+    | 'body1'
+    | 'body2'
+    | 'caption1-semi-bold'
+    | 'caption2'
+    | undefined;
+  color?: TColor;
   disabled?: boolean;
   state?: null | 'disable';
-  className?: string | (string & {});
+  className?: string;
   onClick?: () => void;
-  element?: 'span' | 'p' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5';
+  element?: 'p' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5';
 }
 
 const Text: React.FC<TextProps> = ({
